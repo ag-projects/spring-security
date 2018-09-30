c0b45dcf70d52605b966b01a47343ae710afc8db
Armen G <gharibi.armen@yahoo.com>
2018-09-30 10:12:55 -0700

    URL authorization logic

--- src/main/java/com/agharibi/springsecurity/spring/SecurityConfig.java
+++ src/main/java/com/agharibi/springsecurity/spring/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.agharibi.springsecurity.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -21,4 +22,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         auth.inMemoryAuthentication().withUser(userDetails);
     }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/delete/**")
+                .hasAuthority("ADMIN");
+
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+    
 }
