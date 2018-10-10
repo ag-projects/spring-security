403c01d2213b26835800287f989a7d0196ecdd69
Armen G <gharibi.armen@yahoo.com>
2018-10-09 18:04:41 -0700

    Added remember me saved data in DB

--- src/main/java/com/agharibi/springsecurity/spring/SecurityConfig.java
+++ src/main/java/com/agharibi/springsecurity/spring/SecurityConfig.java
@@ -2,14 +2,22 @@ package com.agharibi.springsecurity.spring;
 
 import com.agharibi.springsecurity.security.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
@@ -56,13 +64,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .csrf().disable();
 
+//        http
+//                .rememberMe()
+//                .tokenValiditySeconds(604800)  // a week
+//                .key("MyAppKey")
+////                .useSecureCookie(true);
+//                .rememberMeCookieName("sticky-cookie")
+//                .rememberMeParameter("remember");
+
         http
                 .rememberMe()
-                .tokenValiditySeconds(604800)  // a week
-                .key("MyAppKey")
-//                .useSecureCookie(true);
-                .rememberMeCookieName("sticky-cookie")
-                .rememberMeParameter("remember");
+                .tokenRepository(persistentTokenRepository());
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
     }
 
 }
