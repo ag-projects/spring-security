7553f7a27a53442576389943b91c0d2800bdbc5c
Armen G <gharibi.armen@yahoo.com>
2018-10-12 21:56:15 -0700

    Fixed the nav bar

--- src/main/java/com/agharibi/springsecurity/spring/SecurityConfig.java
+++ src/main/java/com/agharibi/springsecurity/spring/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
